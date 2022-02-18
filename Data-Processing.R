89d26c3da6a4a851b44019c25b9df82a9033eee9
Zachary Himmelberger <zhimmel@gmail.com>
2022-02-18 16:02:48 -0500

    update path to data file

--- Data-Processing.R
+++ Data-Processing.R
@@ -7,8 +7,7 @@ library(readxl)
 library(tidyverse)
 
 # Load Data
-
-raw.df <- read_excel("/Users/zach.himmelberger/OneDrive - Maryville College/Research/Personality and Attitudes/attitudes_final.xlsx", 
+raw.df <- read_excel("data/attitudes_final.xlsx", 
                      sheet=1, 
                      skip=1)
 head(raw.df, n=10)
